dd7174971b4321d62f4e785c1e1dc7606a4ade9a
Fatima <fatima.trtak@abc-techgroup.com>
2020-10-19 15:32:32 +0200

    Add Contact Info properties for Owners. Closes  #28.

--- pet-clinic-data/src/main/java/com/spring/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/spring/petclinic/model/Owner.java
@@ -1,10 +1,15 @@
 package com.spring.petclinic.model;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
 }
