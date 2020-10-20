f4d87d2699e343cd9057693d64350cee0c04344f
Fatima <fatima.trtak@abc-techgroup.com>
2020-10-20 15:47:02 +0200

    Convert vets to JPA entities. Closes #37.

--- pet-clinic-data/src/main/java/com/spring/petclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/spring/petclinic/model/Specialty.java
@@ -3,8 +3,17 @@ package com.spring.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "specialty")
 public class Specialty extends BaseEntity {
+
+    @Column(name = "description")
     private String description;
+
 }
